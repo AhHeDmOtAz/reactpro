762eb60eb3737bfe7f765f7d00a03bfb4fb0c097
AhHeDmOtAz <ahmedmotez444@gmail.com>
2022-03-16 12:17:22 +0200

    add file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { BrowserRouter } from 'react-router-dom';
-
+       
 
 ReactDOM.render(
  <BrowserRouter>
